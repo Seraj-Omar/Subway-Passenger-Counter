b576ac36e1ae457f8c27596cbcffcf983e4b9105
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2025-05-14 00:35:40 +0300

    Added via uploads

---
+++ script.js
@@ -0,0 +1,21 @@
+let count=0
+let x=0
+let countPs=document.getElementById("count-ps")
+let countEn=document.getElementById("count-en")
+
+function increment(){
+    count++
+    countPs.innerText=count
+}
+
+function save(){
+    if(count!=0){
+        if(x!=0){
+            countEn.innerText+=" -"
+        }
+        countEn.innerText+=" "+count
+        x++
+        count=0;
+        countPs.innerText=0
+    }
+}
